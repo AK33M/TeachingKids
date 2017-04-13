9a625ad173750093b5406ce9f81396586f22e98d
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-04-13 16:35:12 +0100

    initial commit

---
+++ TeachingKids/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachingKids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
